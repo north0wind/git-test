05e47e704da2d906728a112f485bfdb0a7cccfe1
Wind <Wind@qq.com>
2022-06-09 21:06:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @Author: xiaolong
+ * @Date: 2022-06-09 20:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
